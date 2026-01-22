bcd04c5df2b5cd5db71ec3cee1d257be1c011636
CIPL1759 <CIPL1759@CIPL-ATC00591>
2026-01-22 11:02:30 +0530

    Leetcode 11th sum Updated with Two Pointer pattern

--- Csharp.Test/Leetcode/11.Container-With-Most-WaterTests.cs
+++ Csharp.Test/Leetcode/11.Container-With-Most-WaterTests.cs
@@ -13,8 +13,11 @@ namespace Csharp.Test.Leetcode
             var solution = new Csharp_Exercise.Leecodes();
             var MockData = new ArraryMock();
             // Act & Assert
+           // Assert.Equal(49, solution.MaxArea1([2, 3, 4, 4]));
+            Assert.Equal(49, solution.MaxArea1(MockData.Intarrary));
             Assert.Equal(8, solution.MaxArea(MockData.Intarrary));
             Assert.Equal(8, solution.MaxArea([2,3,4,4]));
+            
         }
     }
 }
--- Csharp_Exercise/Leetcode/11.Container-With-Most-Water.cs
+++ Csharp_Exercise/Leetcode/11.Container-With-Most-Water.cs
@@ -17,15 +17,13 @@ namespace Csharp_Exercise
         Return the maximum amount of water a container can store.
         Notice that you may not slant the container.
          */
-        public int MaxArea(int[] height)
+        public int MaxArea(int[] height) // traditional way to compare the area to max area.  O(N^2) Time Limit Exceeded Exception
         {
             /*
              * it act just like rectangle container, need to find maximum area of this.
              * Area of the Rectangle = Length * Height
              * MaxArea = ( Length[j].index - Length[i].index ) * height[j]
              */
-
-            // traditional way to compare the area to max area.  O(N^2) Time Limit Exceeded Exception
            // height = [1, 1];
             int MaxArea = 0;
             for (int i = 1; i <= height.Length; i++)
@@ -44,6 +42,83 @@ namespace Csharp_Exercise
             }
 
             return MaxArea;
+        }
+
+        public int MaxArea1(int[] height) // Two Pointers pattern o(n)
+        {
+            /*
+             *  set two pointer (left) and (right)
+             *  if after calculate area then compare two pointer height and adjust the pointer with minimum height
+             *  pointer inward.
+             */
+            int result = 0;
+            int left = 0;
+            int right = height.Length -1;
+            while (left < right){
+
+                int temp = (right - left) * (Math.Min(height[right], height[left]));
+                if (result < temp)
+                {
+                    result = temp;
+                }
+
+                if (height[left] > height[right])
+                    right--;
+                else
+                    left++;
+            }
+
+            return result;
+        }
+        public int MaxArea2(int[] height) // best case (time)
+        {
+            int l = 0;
+            int r = height.Length - 1;
+            int res = 0;
+            while (l < r && res < (r - l) * 10000)
+            {
+                res = Math.Max(res, height[l] > height[r] ? height[r] * (r - l) : height[l] * (r - l));
+                if (height[l] > height[r]) r--;
+                else l++;
+            }
+            return res;
+        }
+
+        public int MaxArea3(int[] height) // best case (space)
+        {
+            var right = new int[height.Length];
+
+            right[height.Length - 1] = height[height.Length - 1];
+            for (var i = height.Length - 2; i >= 0; i--)
+            {
+                right[i] = Math.Max(height[i], right[i + 1]);
+            }
+
+            var maxArea = 0;
+            var maxLeft = 0;
+            for (var i = 0; i < height.Length; i++)
+            {
+                if (maxLeft >= right[i])
+                {
+                    break;
+                }
+                if (height[i] <= maxLeft)
+                {
+                    continue;
+                }
+                maxLeft = height[i];
+                var maxRight = 0;
+                for (var j = height.Length - 1; j > i; j--)
+                {
+                    if (height[j] <= maxRight)
+                    {
+                        continue;
+                    }
+                    maxArea = Math.Max(maxArea, (j - i) * Math.Min(height[i], height[j]));
+                }
+            }
+
+            return maxArea;
         }
     }
 }
