fbd0b35e3c199a4b4438f109c00bb737e4bb11b4
CIPL1759 <CIPL1759@CIPL-ATC00591>
2026-01-23 12:20:02 +0530

    leetcode 3sum Two pointer workout

---
+++ Csharp.Test/Leetcode/15.3Sum.cs
@@ -0,0 +1,19 @@
+﻿using Csharp.Test.MockInput;
+using Csharp_Exercise;
+using Xunit;
+
+namespace Csharp.Test.Leetcode
+{
+    public class  Sum3
+    {
+        [Fact]
+        public void Add3Sum()
+        {
+            // Arrange
+            var solution = new Csharp_Exercise.Leecodes();
+
+            // Act & Assert
+            Assert.Equal([[-1, -1, 2], [-1, 0, 1]], solution.ThreeSum([-1, 0, 1, 2, -1, -4]));
+        }
+    }
+}
---
+++ Csharp_Exercise/Leetcode/15.3Sum.cs
@@ -0,0 +1,46 @@
+﻿using Csharp_Exercise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Exercise
+{
+    public partial class Leecodes
+    {
+        /*
+         * In Transition method, we can use 3 nested loop but it leads O(n^3) time complexity.
+         * so, try to do with O(n^2) complexity.
+         */
+        public IList<IList<int>> ThreeSum(int[] nums) // Two pointer method
+        {
+            IList<IList<int>>result = new List<IList<int>>();
+            Array.Sort(nums); // first the no. makes easy to handle duplicate values.
+            int k = nums.Length - 1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int j = i + 1;
+                if (i >0 && nums[i] == nums[i-1])
+                {
+                    continue; // skip the duplicate from i
+                }
+                while (j < k)
+                {
+                    int total = nums[i] + nums[j] + nums[k];
+                    if (total == 0)
+                    {
+                        result.Add(new List<int> { nums[i], nums[j], nums[k] });
+                        j++;
+                        k--;
+                        while (j<k && nums[j] == nums[j - 1]) j++; // skip the duplicate from j
+                        while (j < k && nums[k] == nums[k + 1]) k--; // skip the duplicate from k
+                    }
+                    else if (total < 0) j++; // move j towards
+                    else k--; // move k inwards.
+                }
+            }
+            return result;
+        }
+    }
+}
