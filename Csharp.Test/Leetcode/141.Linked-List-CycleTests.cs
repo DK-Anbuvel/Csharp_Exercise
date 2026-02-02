55e3ed4fa5c3b49ff77fdb14fb6eecdd1dc7d900
CIPL1759 <CIPL1759@CIPL-ATC00591>
2026-02-02 19:21:50 +0530

    leetcode 141 Hashset/single linked list

---
+++ Csharp.Test/Leetcode/141.Linked-List-CycleTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp.Test.Leetcode
+{
+    public class Test141
+    {
+        /**
+ * Definition for singly-linked list. **/
+  public class ListNode1 {
+      public int val;
+      public ListNode1 next;
+      public ListNode1(int x) {
+          val = x;
+          next = null;
+      }
+  }
+
+        [Fact]
+        public void HasCycleTest()
+        {
+            // Arrange
+            var solution = new Csharp_Exercise.Leecodes();
+
+            //Act
+            var node1 = new ListNode1(2);
+            var node2 = new ListNode1(22);
+            node1.next = node2;
+            node2.next = node1; // create cycle
+
+            // Assert
+            Assert.Equal(true, solution.HasCycle());
+        }
+    }
+}
---
+++ Csharp_Exercise/Leetcode/141.Linked-List-Cycle.cs
@@ -0,0 +1,52 @@
+﻿using Csharp_Exercise;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Exercise
+{
+    public partial class Leecodes
+    {/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+        public class ListNode1
+        {
+            public int val;
+            public ListNode1 next;
+            public ListNode1(int x)
+            {
+                val = x;
+                next = null;
+            }
+        }
+        public bool HasCycle() // single linked list HashSet used to store unique value in shorted manner.
+        {
+            var node1 = new ListNode1(2);
+            var node2 = new ListNode1(22);
+            node1.next = node2;
+            node2.next = node1; // create cycle
+            ListNode1 head = node1;
+            HashSet<ListNode1> vistedNode = new HashSet<ListNode1>(); 
+           // ListNode currentNode = head;
+            while(head != null)
+            {
+                if (vistedNode.Contains(head)) return true;
+                vistedNode.Add(head);
+                
+                head = head.next;
+            }
+            return false;
+        }
+    }
+}
