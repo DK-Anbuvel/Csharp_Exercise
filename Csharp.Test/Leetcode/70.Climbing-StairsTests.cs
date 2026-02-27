85f3ae005020d4e16d08adbd13f253b55893ef0b
CIPL1759 <CIPL1759@CIPL-ATC00591>
2026-02-27 18:46:14 +0530

    leetcode 70

---
+++ Csharp.Test/Leetcode/70.Climbing-StairsTests.cs
@@ -0,0 +1,14 @@
+﻿namespace Csharp.Test.Leetcode
+{
+    public class Test70
+    {
+        [Fact]
+        public void ClimbStairsTest()
+        {
+            // Arrange
+            var solution = new Csharp_Exercise.Leecodes();
+
+            Assert.Equal(3, solution.ClimbStairs(3));
+        }
+    }
+}
---
+++ Csharp_Exercise/Leetcode/70.Climbing-Stairs.cs
@@ -0,0 +1,42 @@
+﻿namespace Csharp_Exercise
+{
+    public partial class Leecodes
+    {
+        public int ClimbStairs(int n) //Memoization O(n) O(n)
+        {
+            int[] dp = new int[n + 1];
+            Array.Fill(dp, -1);
+            int k = ClimbStairsHelper(n, dp);
+            return k;
+        }
+        public int ClimbStairsHelper(int n, int[] dp)
+        {
+            if (n <= 1) return 1;
+            if (dp[n] != -1) return dp[n];
+            dp[n] = ClimbStairsHelper(n - 1, dp) +
+                    ClimbStairsHelper(n - 2, dp);
+            return dp[n];
+        }
+        public int ClimbStairs1(int n)  //Tabulation O(n) O(n)
+        {
+            int[] tab = new int[n + 1];
+            if (tab.Length > 0) tab[0] = 1;
+            if (tab.Length > 1) tab[1] = 1;
+            for (int i = 2; i < tab.Length; i++)
+                tab[i] = tab[i - 1] + tab[i - 2];
+            return tab[n];
+        }
+        public int ClimbStairs2(int n) //Space Optimization  O(n) O(1)
+        {
+            if (n <= 1) return 1;
+            int firstNum = 1, secondNum = 1, thirdNum = 0;
+            for (int i = 2; i < n + 1; i++)
+            {
+                thirdNum = firstNum + secondNum;
+                firstNum = secondNum;
+                secondNum = thirdNum;
+            }
+            return thirdNum;
+        }
+    }
+}
