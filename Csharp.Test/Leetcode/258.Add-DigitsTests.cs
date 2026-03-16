e6806b96757ada8830138e5ea198c840392a7954
Anbudhilip <anbudhilip2001@gmail.com>
2026-03-16 18:36:37 +0530

    leetcode 258

---
+++ Csharp.Test/Leetcode/258.Add-DigitsTests.cs
@@ -0,0 +1,15 @@
+﻿namespace Csharp.Test.Leetcode
+{
+    public class Testcase258
+    {
+        [Fact]
+        public void AddDigitsTest()
+        {           // Arrange
+            var solution = new Csharp_Exercise.Leecodes();
+
+            // Act & Assert
+            solution.AddDigits3(38);
+            solution.AddDigits1(388);
+        }
+    }
+}
