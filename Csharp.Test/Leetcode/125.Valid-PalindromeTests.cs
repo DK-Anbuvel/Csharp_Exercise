ec6f352a274186550091316e0be0b3993f3c5a16
CIPL1759 <CIPL1759@CIPL-ATC00591>
2026-04-18 16:18:26 +0530

    review 125

--- Csharp.Test/Leetcode/125.Valid-PalindromeTests.cs
+++ Csharp.Test/Leetcode/125.Valid-PalindromeTests.cs
@@ -11,13 +11,16 @@ namespace Csharp.Test.Leetcode
         [Fact]
         public void IsPalindrome()
         {
-         //   // Arrange
-         //   var solution = new Csharp_Exercise.Leecodes();
+            // Arrange
+            var solution = new Csharp_Exercise.Leecodes();
 
-         //   // Act & Assert
-         ////  Assert.Equal(true, solution.IsPalindrome125(""));
-         //   Assert.Equal(false, solution.IsPalindrome125("race a car"));
-        
-    }
+            // Act & Assert
+            //  Assert.Equal(true, solution.IsPalindrome125(""));
+            Assert.Equal(false, solution.IsPalindrome125VII("P0"));
+            Assert.Equal(false, solution.IsPalindrome125VII("0P"));
+            Assert.Equal(false, solution.IsPalindrome125VII("race a car"));
+            Assert.Equal(false, solution.IsPalindrome125VII("A man, a plan, a canal: Panama"));
+
+        }
     }
 }
--- Csharp_Exercise/Leetcode/125.Valid-Palindrome.cs
+++ Csharp_Exercise/Leetcode/125.Valid-Palindrome.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Csharp_Exercise
@@ -115,6 +116,136 @@ namespace Csharp_Exercise
 
             return newStr == new string(newStr.Reverse().ToArray());
         }
+        public bool IsPalindrome125VI(string s) //Runtime: 34 ms
+        {
+            /*
+               About the problem:-
+                    validate string(letter and digits) is palindrome or not 
+               My approach :-
+                    Two pointers
+                    For alphanumeric characters.
+                       buildin methods :-
+                         Char.IsLetterOrDigit
+                         Char.IsLetter & Char.IsDigit
+                       Manual methods :-
+                           Range
+                           Digits: 48 to 57 ('0' to '9')
+                           Uppercase: 65 to 90 ('A' to 'Z')
+                           Lowercase: 97 to 122 ('a' to 'z') 
+                           Regex [^a-zA-Z0-9]
+                           string "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789". 
+            */
+            // s = Regex.Replace(s, "[^a-zA-Z0-9]");
+            s = s.ToLower();
+            s = Regex.Replace(s, "[^a-z0-9]","");
+            int l = 0, r = s.Length-1;
+            while(l< r)
+            {
+                if (s[l] != s[r]) return false;
+                l++;
+                r--;
+            }
+            return true;
+        }
+        public bool IsPalindrome125VII(string s) //Runtime: 1 ms  
+        {
+            /*
+               About the problem:-
+                    validate string(letter and digits) is palindrome or not 
+               My approach :-
+                    Two pointers
+                    For alphanumeric characters.
+                       Manual methods :-
+                           Range
+                           Digits: 48 to 57 ('0' to '9')
+                           Uppercase: 65 to 90 ('A' to 'Z')
+                           Lowercase: 97 to 122 ('a' to 'z') 
+
+             upper case validate like this 65 + 32 = 97
+            digits,
+
+            "A man, a plan, a canal: Panama"
+            */
+            int l = 0, r = s.Length - 1;
+
+            for (; l < r; )
+            {
+                //if ((s[l] <= 9) || !(s[l] >= 65 && s[l] <= 90) || !(s[l] >= 97 && s[l] <= 122)) // left
+                //    ++l;
+
+                if (!char.IsLetterOrDigit(s[l]))
+                {
+                    l++;
+                    continue;
+                }
+                if (!char.IsLetterOrDigit(s[r]))
+                {
+                    r--;
+                    continue;
+                }
+                // for other then char how skip
+                if ((s[l] == s[r])) // digit and lower char ||  uppercase + 32 = lowercase
+                {
+                    l++; r--;
+                }
+                else if( (s[l] >= 65 && s[r] >= 65))//  failed with 0P and P0
+                {
+                    if ((s[l] + 32 == s[r]) || (s[r] + 32 == s[l]))
+                    {
+                        l++; r--;
+                    }else
+                        return false;
+                }
+                else
+                    return false;
+
+            }
+            return true;
+        }
+        public bool IsPalindrome125VIII(string s)
+        {
+            //string s= input.ToLower();
+            short first = 0;
+            short last = (short)(s.Length - 1);
+            bool valid = true;
+
+
+
+            while (first < last)
+            {
+                if (!checkifvalidchar(s[first])) { first++; continue; }
+                if (!checkifvalidchar(s[last])) { last--; continue; }
+                if (toLowerCase(s[first]) == toLowerCase(s[last]))
+                {
+                    first++;
+                    last--;
+                }
+                else
+                {
+                    valid = false;
+                    break;
+                }
+
+            }
+            return valid;
+
+
+        }
+
+        public bool checkifvalidchar(char c)
+        {
+            //Comparision based on ascii
+            int ascivalue = (int)c;
+            if (ascivalue >= 97 && ascivalue <= 122 || ascivalue >= 65 && ascivalue <= 90 || ascivalue >= 48 && ascivalue <= 57)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public char toLowerCase(char c) =>
+
+            (c is >= 'A' and <= 'Z') ? (char)(c + 32) : c;
     }
 
 
