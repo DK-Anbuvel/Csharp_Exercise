229416cb18fc3874ef32a4d8c6b2faa1f9042f51
Anbudhilip <anbudhilip2001@gmail.com>
2026-06-26 17:17:37 +0530

    LC 142 (linkedlist)

--- Csharp_Exercise/Leetcode/138.Copy-List-with-Random-Pointer.cs
+++ Csharp_Exercise/Leetcode/138.Copy-List-with-Random-Pointer.cs
@@ -15,7 +15,7 @@
                     random = null;
                 }
             }
-        public Node CopyRandomList()
+        public Node CopyRandomList()// time O(n) space O(1)
         {
             /*
              about this problem:-
@@ -34,8 +34,7 @@
                   insert between new node copy in every list of nodes.
                   and set random node
                   finally separate the old and new nodes.
-
-             */
+            */
 
             var node1 = new Node(7);
             var node2 = new Node(13);
@@ -53,8 +52,6 @@
             node3.random = node5;
             node4.random = node3;
             node5.random = node1;
-
-
 
             Node head = node1;
 
@@ -97,6 +94,173 @@
 
             return Result.next;
         }
+        public Node CopyRandomList1(Node head)
+        {
+            if (head == null)
+            {
+                return null;
+            }
 
+            Node newHead = new Node(head.val);
+            Node currentNew = newHead;
+            Node currentOriginal = head.next;
+
+            Dictionary<Node, Node> correspondingNodes = new Dictionary<Node, Node>();
+            correspondingNodes.Add(head, newHead);
+
+            while (currentOriginal != null)
+            {
+                currentNew.next = new Node(currentOriginal.val);
+                currentNew = currentNew.next;
+                correspondingNodes.Add(currentOriginal, currentNew);
+                currentOriginal = currentOriginal.next;
+            }
+
+            currentOriginal = head;
+            currentNew = newHead;
+
+            while (currentOriginal != null)
+            {
+                if (currentOriginal.random != null)
+                {
+                    currentNew.random = correspondingNodes[currentOriginal.random];
+                }
+                currentNew = currentNew.next;
+                currentOriginal = currentOriginal.next;
+            }
+
+            return newHead;
+        }
+        Dictionary<Node, Node> hashMap = new Dictionary<Node, Node>();
+        public Node CopyRandomList2(Node head)
+        {
+            // if(head==null)
+            // {
+            //     return null;
+            // }
+            // Node curr = head;
+            // Dictionary<Node,Node> hashMap = new Dictionary<Node,Node>();
+            // while(curr!=null)
+            // {
+            //     Node newNode = new Node(curr.val);
+            //     hashMap.Add(curr,new Node(curr.val));
+            //     curr = curr.next;
+            // }
+
+            // curr = head;
+            // while(curr!=null)
+            // {
+            //     hashMap[curr].next = curr.next!=null?hashMap[curr.next]:null;
+            //     hashMap[curr].random = curr.random!=null?hashMap[curr.random]:null;
+            //     curr = curr.next;
+            // }
+
+            // return hashMap[head];
+
+            if (head == null)
+            {
+                return null;
+            }
+            if (hashMap.ContainsKey(head) == true)
+            {
+                return hashMap[head];
+            }
+            Node newHead = new Node(head.val);
+            hashMap.Add(head, newHead);
+
+            newHead.next = CopyRandomList2(head.next);
+            newHead.random = CopyRandomList2(head.random);
+
+            return newHead;
+        }
+        public Node CopyRandomList3(Node head)
+        {
+            if (head == null)
+                return head;
+
+            Node ori = head;
+            Node n = new(0);
+            Node r = n;
+
+            List<Node> l1 = new();
+            List<Node> l2 = new();
+
+            while (head != null)
+            {
+                n.val = head.val;
+                n.next = new(0);
+                l1.Add(head);
+                l2.Add(n);
+
+                n = n.next;
+                head = head.next;
+            }
+
+            head = ori;
+            n = r;
+
+            while (head != null)
+            {
+                if (head.random != null)
+                {
+                    var idx = l1.IndexOf(head.random);
+                    if (idx != -1)
+                        n.random = l2[idx];
+                }
+                if (head.next == null)
+                    n.next = null;
+                n = n.next;
+                head = head.next;
+            }
+
+            return r;
+        }
+        public Node CopyRandomList4(Node head)
+        {
+            return CopyNode(head, new());
+        }
+
+        private Node CopyNode(Node node, Dictionary<Node, Node> copies)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+            if (copies.ContainsKey(node))
+            {
+                return copies[node];
+            }
+            Node copy = new(node.val);
+            copies.Add(node, copy);
+            copy.next = CopyNode(node.next, copies);
+            copy.random = CopyNode(node.random, copies);
+            return copy;
+        }
+        public Node CopyRandomList5(Node head)
+        {
+            if (head is null) return head;
+            Dictionary<Node, Node> dic = new()
+            {
+                [head] = new(head.val)
+            };
+            var curr = head;
+            while (curr is not null)
+            {
+                if (curr.next is not null && !dic.ContainsKey(curr.next))
+                    dic[curr.next] = new(curr.next.val);
+                if (curr.next is not null)
+                    dic[curr].next = dic[curr.next];
+                if (curr.random is not null)
+                {
+                    if (!dic.ContainsKey(curr.random))
+                    {
+                        dic[curr.random] = new(curr.random.val);
+                    }
+                    dic[curr].random = dic[curr.random];
+                }
+                curr = curr.next;
+            }
+            return dic[head];
+        }
     }
 }
---
+++ Csharp_Exercise/Leetcode/142.Linked-List-Cycle-II.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace Csharp_Exercise
+{
+    public partial class Leecodes
+    {
+        public ListNode DetectCycle(ListNode head) // 283 ms time o(n^2) space o(n)
+        {
+
+            /*
+              About this problem:-
+                given linked list , return cycle start head node.
+
+              My approach:- [3,2,0,-4,2]
+
+                  Failed: [3,2,2,0,-4] dictionary key stores unique value
+                     Here while iterate and store the node value in dictionary 
+                     till find the match node finded or become null.
+            */
+            if (head == null) return null;
+
+            List<ListNode> track = new List<ListNode>(); //o(n)
+            ListNode currentNode = head;
+            while (currentNode != null) //o(n)
+            {
+
+                if (track.Contains(currentNode)) // o(n)
+                    return currentNode;
+
+                track.Add(currentNode);
+                currentNode = currentNode.next;
+            }
+
+            return null;
+        }
+    }
+}
