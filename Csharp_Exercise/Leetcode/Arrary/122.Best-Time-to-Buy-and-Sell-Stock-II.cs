5a1728d65d0690864bbc08f5d560286ebe5aff4a
Anbudhilip <anbudhilip2001@gmail.com>
2026-08-31 19:29:18 +0530

    LC 122

---
+++ Csharp_Exercise/Leetcode/Arrary/122.Best-Time-to-Buy-and-Sell-Stock-II.cs
@@ -0,0 +1,32 @@
+﻿namespace Csharp_Exercise
+{
+    public partial class Leecodes
+    {
+        public partial class arrayProblem
+        {
+            public int MaxProfit(int[] prices) // time O(n) space O(1)
+            { // [5,2,3,4,5]
+                /*
+                 about this problem:-
+                    return max profit,
+                    buy and sell not happend in backward.
+
+                 My approach:-
+                    greedy algorithm:-
+                     profit += hight no. - smallest no.
+                */
+                int profit = 0;
+
+                for (int i = 1; i < prices.Length; i++)
+                {
+
+                    if (prices[i] > prices[i - 1])
+                        profit += prices[i] - prices[i - 1];
+
+                }
+
+                return profit;
+            }
+        }
+    }
+}
