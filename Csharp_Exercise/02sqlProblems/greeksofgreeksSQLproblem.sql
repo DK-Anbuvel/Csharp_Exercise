96b3e6393be22db2c029aed1e37fc6ba4fedaa08
Anbudhilip <anbudhilip2001@gmail.com>
2026-09-03 19:32:33 +0530

    greeks sql problems

--- Csharp_Exercise/02sqlProblems/greeksofgreeksSQLproblem.sql
+++ Csharp_Exercise/02sqlProblems/greeksofgreeksSQLproblem.sql
@@ -2,35 +2,56 @@
 /* https://www.geeksforgeeks.org/sql/sql-query-interview-questions/ */
 
 
-create table Student (
- STUDENT_ID INT primary key,
- FIRST_NAME VARCHAR(30),
- LAST_NAME VARCHAR(30),
- GPA INT,
- ENROLLMENT_DATE DATETIME,
- MAJOR VARCHAR(30)
-)
-
-CREATE table Program(
-STUDENT_REF_ID INT ,
- PROGRAM_NAMES VARCHAR(30),
- PROGRAM_START_DATE DATETIME,
-)
-
-CREATE table Scholarship(
-STUDENT_REF_ID INT ,
- SCHOLARSHIP_AMOUNT VARCHAR(30),
- SCHOLARSHIP_DATE DATETIME,
-)
-
-
 /* 1. Write a SQL query to fetch "FIRST_NAME" from the Student table in upper case and use ALIAS name as STUDENT_NAME. */
 
-select upper(FIRST_NAME) as STUDENT_NAME from Student
+select upper(FIRSTNAME) as STUDENT_NAME from Students
 
 
 /* 2. Write a SQL query to fetch unique values of MAJOR Subjects from Student table. */
 
-select distinct MAJOR from Student
-select MAJOR from Student group by MAJOR
+select distinct MAJOR from Students
+select MAJOR from Students group by MAJOR
+
+/* 3. Write a SQL query to print the first 3 characters of FIRST_NAME from Student table. */
+
+/* extracting the first 3 character from firstname used by SUBSTRING() function */
+select	SUBSTRING(Firstname,1,3),FirstName from students
+
+/*4. Write a SQL query to find the position of alphabet ('a') int the first name column 'Grace' from Student table. */
+
+select charIndex('a',lower(firstName)),FirstName from Students where FirstName = 'Grace';
+
+/* 5. Write a SQL query that fetches the unique values of MAJOR Subjects from Student table and print its length.*/
+
+select distinct MAJOR, LEN(Major) from Students
+select MAJOR ,  LEN(Major)from Students group by MAJOR
+
+/* 6. Write a SQL query to print FIRST_NAME from the Student table after replacing 'a' with 'A'.*/
+
+  /*STUFF() function is used to delete a sepecific lenght of characters from string
+   and insert a new substring at that exact position
+   STUFF(original_string, start_position, length_to_delete, replacement_string)
+
+   REPLACE() function is used to replace an instance of a character anywhere it appears in a string,
+
+*/
+select Stuff(firstname,charIndex('a',firstName),1,'A'),FirstName from students
+select REPLACE(firstname,'a','A'),firstname from students 
+
+/*7. Write a SQL query to print the FIRST_NAME and LAST_NAME from Student table into single column COMPLETE_NAME. */
+
+select FirstName+' '+LastName as COMPLETE_NAME,firstname,LastName from Students
+select CONCAT(FirstName,' ',lastName) as COMPLETE_NAME,firstname,LastName from Students
+
+/*8. Write a SQL query to print all Student details from Student table order by FIRST_NAME Ascending and MAJOR Subject descending . */
+
+select * from Students order by FirstName, major desc
+
+/*9. Write a SQL query to print details of the Students with the FIRST_NAME as 'Noah' and 'Ava' from Student table */
+
+select * from Students where FirstName in ('Noah','Ava')
+
+/*10. Write a SQL query to print details of the Students excluding FIRST_NAME as 'Jack' and 'Liam' from Student table */
+
+select * from Students where FirstName not in ('Jack','Liam')
 
---
+++ Csharp_Exercise/MockData/greeksofgreeksSQLdbTables.sql
@@ -0,0 +1,103 @@
+﻿-- =========================
+-- 1. STUDENT TABLE
+-- =========================
+
+CREATE TABLE Students
+(
+    StudentId INT PRIMARY KEY,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    GPA DECIMAL(4,2),
+    EnrollmentDate DATETIME NOT NULL,
+    Major VARCHAR(100)
+);
+
+
+INSERT INTO Students
+(
+    StudentId,
+    FirstName,
+    LastName,
+    GPA,
+    EnrollmentDate,
+    Major
+)
+VALUES
+(201, 'Jack',  'Smith',  8.79, '2021-09-10 09:30:00', 'Computer Science'),
+(202, 'Ryan',  'Brown',  8.44, '2021-09-10 08:30:00', 'Mathematics'),
+(203, 'Ethan', 'Davis',  5.60, '2021-09-10 10:00:00', 'Biology'),
+(204, 'Emma',  'Wilson', 9.20, '2021-09-12 12:45:00', 'Chemistry'),
+(205, 'Noah',  'Miller', 7.85, '2021-09-10 08:30:00', 'Physics'),
+(206, 'Liam',  'Taylor', 9.56, '2021-09-10 09:24:00', 'History'),
+(207, 'Ava',   'Moore',  9.78, '2021-09-01 02:30:00', 'English'),
+(208, 'Grace', 'Clark',  7.00, '2021-09-10 06:30:00', 'Mathematics');
+
+
+-- =========================
+-- 2. STUDENT PROGRAM TABLE
+-- =========================
+
+CREATE TABLE StudentPrograms
+(
+    StudentRefId INT PRIMARY KEY,
+    ProgramName VARCHAR(100) NOT NULL,
+    ProgramStartDate DATETIME NOT NULL,
+
+    CONSTRAINT FK_StudentPrograms_Students FOREIGN KEY (StudentRefId)
+        REFERENCES Students(StudentId)
+);
+
+
+INSERT INTO StudentPrograms
+(
+    StudentRefId,
+    ProgramName,
+    ProgramStartDate
+)
+VALUES
+(201, 'Computer Science', '2021-09-01 00:00:00'),
+(202, 'Mathematics',      '2021-09-01 00:00:00'),
+(208, 'Mathematics',      '2021-09-01 00:00:00'),
+(205, 'Physics',          '2021-09-01 00:00:00'),
+(204, 'Chemistry',        '2021-09-01 00:00:00'),
+(207, 'Psychology',       '2021-09-01 00:00:00'),
+(206, 'History',          '2021-09-01 00:00:00'),
+(203, 'Biology',          '2021-09-01 00:00:00');
+
+
+-- =========================
+-- 3. SCHOLARSHIP TABLE
+-- =========================
+
+CREATE TABLE Scholarships
+(
+    StudentRefId INT PRIMARY KEY,
+    ScholarshipAmount DECIMAL(10,2) NOT NULL,
+    ScholarshipDate DATETIME NOT NULL,
+
+    CONSTRAINT FK_Scholarships_Students
+        FOREIGN KEY (StudentRefId)
+        REFERENCES Students(StudentId)
+);
+
+
+INSERT INTO Scholarships
+(
+    StudentRefId,
+    ScholarshipAmount,
+    ScholarshipDate
+)
+VALUES
+(201, 5000, '2021-10-15 00:00:00'),
+(202, 4500, '2022-08-18 00:00:00'),
+(203, 3000, '2022-01-25 00:00:00'),
+(204, 4000, '2021-10-15 00:00:00');
+
+SELECT *
+FROM Students;
+
+SELECT *
+FROM StudentPrograms;
+
+SELECT *
+FROM Scholarships;
